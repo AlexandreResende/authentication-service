802b277b980348b9dab3bfcf31ec06fc4a266293
AlexandreResende <alexandre.amres@gmail.com>
2025-06-04 23:06:01 -0300

    docs: Add swagger description for health-check endpoint

--- src/routers/healthcheckRouter.js
+++ src/routers/healthcheckRouter.js
@@ -5,6 +5,20 @@ const errorHandler = require('../middlewares/errorHandler');
 
 const healthcheckRouter = express.Router();
 
-healthcheckRouter.get('/health-check', errorHandler(container.resolve('healthCheckController')));
+healthcheckRouter.get('/health-check', errorHandler(container.resolve('healthCheckController')), (req, res, next) => {
+   /*  #swagger.start
+  
+      #swagger.path = '/health-check'
+      #swagger.method = 'get'
+      #swagger.description = 'This endpoint checks the health of the application'
+      #swagger.produces = ['application/json']
+      #swagger.consumes = ['application/json']
+  
+      #swagger.responses[200]
+      #swagger.responses[500]
+      #swagger.end
+  */
+
+});
 
 module.exports = healthcheckRouter;
